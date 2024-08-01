cd134739d59268768e6e9f8eb383d2ceacad5e09
Marina Muñoz <marinacerejidoo@gmail.com>
2024-08-01 21:22:31 +0200

    Human move

--- src/components/ChessGame.jsx
+++ src/components/ChessGame.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Chessboard from "chessboardjsx"; // Import the chessboard from chessboardjsx library
 import { Chess } from "chess.js"; // Import the Chess class from chess.js
-import stockfish from "stockfish"; // Import stockfish that will serve as the bot to play against
+
+// We can't import stockfish here because Vite doesn't support it, that's why we rely on the window.stockfish later
 
 const ChessGame = () => {
   // Create a new chess game instance
@@ -18,34 +19,55 @@ const ChessGame = () => {
 
   useEffect(() => {
     //Initialize stockfish engine
-    stockfishRef.current = stockfish();
+    if (window.Stockfish) {
+      stockfishRef.current = window.Stockfish();
 
-    //Check what stockfish considers the best move
-    stockfishRef.current.onmessage = (event) => {
-      if (event && event.startsWith("bestmove")) {
-        // Extract the numerical position with its index since it comes as "best-move e4"
-        const bestMove = event.split(" ")[1];
+      //Check what stockfish considers the best move
+      stockfishRef.current.onmessage = (event) => {
+        if (event && event.startsWith("bestmove")) {
+          // Extract the numerical position with its index since it comes as "best-move e4"
+          const bestMove = event.split(" ")[1];
 
-        //Make the move and Set that best move as its new position
-        chessGame.move(bestMove);
-        setPosition(chessGame.fen());
-        //Set that the bot is not thinking any longer
-        setIsBotThinking(false)
-      }
-    };
+          //Make the move and Set that best move as its new position
+          chessGame.move(bestMove);
+          setPosition(chessGame.fen());
+          //Set that the bot is not thinking any longer
+          setIsBotThinking(false);
+        }
+      };
 
-    //Terminate stockfish when the game is over
-    return() => {
-      stockfishRef.current.terminate()
+      //Terminate stockfish when the game is over
+      return () => {
+        stockfishRef.current.terminate();
+      };
     }
   }, []);
 
   //HOW THE HUMAN MOVES
-  
+  const onDrop = ({ sourceSquare, targetSquare }) => {
+    const move = chessGame.move({
+      from: sourceSquare,
+      to: targetSquare,
+      promotion: "q",
+    });
+
+    //if they make a move that isn't possible, exit
+    if (move === null) return;
+
+    setPosition(chessGame.fen());
+    setIsBotThinking(true);
+
+    stockfishRef.current.postMessage(`position fen ${chessGame.fen()}`);
+    stockfishRef.current.postMessage("go depth 15"); //this is the command to instruct stockfish to start calculating the next best move
+  };
 
   return (
     <>
       <h1>Hi from chessboard</h1>
+      <Chessboard
+        position={position}
+        onDrop={onDrop} // Attach the onDrop handler to the Chessboard component
+      />
     </>
   );
 };
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,8 +1,12 @@
+import ChessGame from "../components/ChessGame";
+import { Link } from "react-router-dom"; // Import Link for navigation
+
 const Homepage = () => {
   return (
     <>
       <h1>Hola mongoloooo💖</h1>
       <Link to="/play">¿Una partidita?</Link>
+      <ChessGame />
     </>
   );
 };
